19ba7b5f23e932ed92a27509373cce0f93f5485b
amirul-mamun <amirulislam21354>
2025-04-24 11:02:50 +0600

    some updated file

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log('its working!');
+
+let btn = document.querySelector("#btn")
+
+let remove = () => {
+    
+    btn.classList.add("rm")
+    console.log("element removed!");
+    
+    
+}
+
+btn.addEventListener("click", remove)
+
+let obj = {
+    name: "mamun",
+    age: 15,
+    status: "Active",
+
+    walk(d){
+        console.log(`Wallked for ${d}`); 
+    },
+
+    speak(s){
+        console.log(`speaked for ${s}`); 
+    }
+}
